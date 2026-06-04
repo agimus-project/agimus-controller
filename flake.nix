d178a57bfcf71b3318c6e5d67622042368c8b3d7
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-06-04 17:13:28 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -1,60 +1,33 @@
 {
   description = "Whole Body Model Predictive Control in the AGIMUS architecture";
 
-  inputs = {
-    gazebros2nix.url = "github:gepetto/gazebros2nix";
-    flake-parts.follows = "gazebros2nix/flake-parts";
-    nixpkgs.follows = "gazebros2nix/nixpkgs";
-    nix-ros-overlay.follows = "gazebros2nix/nix-ros-overlay";
-    systems.follows = "gazebros2nix/systems";
-    treefmt-nix.follows = "gazebros2nix/treefmt-nix";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = [ "x86_64-linux" ];
-        imports = [
-          inputs.gazebros2nix.flakeModule
-          {
-            gazebros2nix.rosPackages = {
-              agimus-controller = _final: _ros-final: {
-                src = lib.fileset.toSource {
-                  root = ./.;
-                  fileset = lib.fileset.unions [
-                    ./agimus_controller
-                  ];
-                };
-              };
-
-              # agimus-controller-examples = _final: _ros-final: {
-              #   src = lib.fileset.toSource {
-              #     root = ./.;
-              #     fileset = lib.fileset.unions [
-              #       ./agimus_controller_examples
-              #     ];
-              #   };
-              # };
-
-              agimus-controller-ros = _final: _ros-final: {
-                src = lib.fileset.toSource {
-                  root = ./.;
-                  fileset = lib.fileset.unions [
-                    ./agimus_controller_ros
-                  ];
-                };
-              };
-
+        rosOverrideAttrs = {
+          agimus-controller = {
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = lib.fileset.unions [
+                ./agimus_controller
+              ];
             };
-          }
-        ];
-        perSystem =
-          { self', ... }:
-          {
-            packages.default = self'.packages.ros-rolling-agimus-controller-ros;
           };
+
+          agimus-controller-ros = {
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = lib.fileset.unions [
+                ./agimus_controller_ros
+              ];
+            };
+          };
+
+        };
       }
     );
 }
